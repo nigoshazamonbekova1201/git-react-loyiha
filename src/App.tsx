4c548cd6868721b7c0ff64a96349f333da9f5175
Nigora <zamombekovanigora1192@gmail.com>
2026-05-22 23:33:28 +0500

    ozgarishlar qilindi

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      
+      <h2>Salom</h2>
+    <p></p>
+    
+  
+    
     </>
   )
 }
